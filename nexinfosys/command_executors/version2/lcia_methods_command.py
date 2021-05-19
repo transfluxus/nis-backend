de979beb24582a1f4b46dab0fe9d379b576978eb
Rafael Nebot Medina <rnebot@gmail.com>
2021-05-19 15:44:41 +0100

    LCIAMethods command creates default InterfaceTypes and Indicators
    if they do not exist

--- nexinfosys/command_executors/version2/lcia_methods_command.py
+++ nexinfosys/command_executors/version2/lcia_methods_command.py
@@ -5,7 +5,7 @@ from nexinfosys.command_generators import Issue, IssueLocation, IType
 from nexinfosys.common.helper import strcmp, first, PartialRetrievalDictionary
 from nexinfosys.model_services import IExecutableCommand, get_case_study_registry_objects
 from nexinfosys.models.musiasem_concepts import Observer, FactorTypesRelationUnidirectionalLinearTransformObservation, \
-    FactorType, Processor, Indicator
+    FactorType, Processor, Indicator, IndicatorCategories, Hierarchy
 from nexinfosys.command_executors import BasicCommand, CommandExecutionError, subrow_issue_message
 from nexinfosys.command_field_definitions import get_command_fields_from_class
 from nexinfosys.models.musiasem_concepts_helper import find_or_create_observer, find_processor_by_name
@@ -22,20 +22,58 @@ class LCIAMethodsCommand(BasicCommand):
         :return:
         """
         # InterfaceType must exist
-        try:
-            self._get_factor_type_from_field(None, "interface")
-        except CommandExecutionError as e:
-            self._add_issue(IType.ERROR, str(e))
+        interface_type = self._glb_idx.get(FactorType.partial_key(fields["interface"]))
+        if len(interface_type) == 1:
+            pass
+        elif len(interface_type) == 0:
+            ft_name = fields["interface"]
+            ft_h_name = "LCIAInterfaceTypes"
+            hie = self._glb_idx.get(Hierarchy.partial_key(name=ft_h_name))
+            if not hie:
+                hie = Hierarchy(name=ft_h_name, type_name="interfacetype")
+                self._glb_idx.put(hie.key(), hie)
+            else:
+                hie = hie[0]
+
+            # Create it and warn
+            ft = FactorType(ft_name,
+                            parent=None, hierarchy=hie,
+                            roegen_type=None,
+                            tags=None,  # No tags
+                            attributes=dict(unit=fields["interface_unit"], description=ft_name, level=None),
+                            expression=None,
+                            sphere=None,
+                            opposite_processor_type=None
+                            )
+            # Simple name
+            self._glb_idx.put(FactorType.partial_key(ft_name, ft.ident), ft)
+            self._add_issue(IType.WARNING, f"InterfaceType with name '{ft_name}' not found, created" + subrow_issue_message(subrow))
+        else:
+            self._add_issue(IType.ERROR,
+                            f"InterfaceType with name '{fields['interface']}' found {len(interface_type)} times" + subrow_issue_message(subrow))
+            return
 
         # (LCIA) Indicator must exist
         indicator = self._glb_idx.get(Indicator.partial_key(fields["lcia_indicator"]))
         if len(indicator) == 1:
             pass
         elif len(indicator) == 0:
-            self._add_issue(IType.ERROR, f"Indicator with name '{fields['lcia_indicator']}' not found" + subrow_issue_message(subrow))
-            return
+            # Create it and warn
+            indicator = Indicator(fields["lcia_indicator"],
+                                  f'LCIAMethods("{fields["lcia_indicator"]}")',
+                                  None,
+                                  "",
+                                  [],
+                                  IndicatorCategories.factors_expression,
+                                  "",
+                                  "LCIA",
+                                  fields["interface_unit"],
+                                  fields["interface_unit"],
+                                  fields["lcia_method"])
+            self._glb_idx.put(indicator.key(), indicator)
+            self._add_issue(IType.WARNING, f"Indicator with name '{fields['lcia_indicator']}' not found, created" + subrow_issue_message(subrow))
         else:
-            self._add_issue(IType.WARNING,
+            self._add_issue(IType.ERROR,
                             f"Indicator with name '{fields['lcia_indicator']}' found {len(indicator)} times" + subrow_issue_message(subrow))
             return
 
