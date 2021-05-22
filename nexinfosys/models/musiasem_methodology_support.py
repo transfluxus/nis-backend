853193e7afab4f2383c3bc65fb7cee4ec242c5aa
Rafael Nebot Medina <rnebot@gmail.com>
2021-05-22 20:42:52 +0100

    Enabled to avoid an SQLAlchemy warning
    True assures SQLAlchemy that the GUID type is cacheable

--- nexinfosys/models/musiasem_methodology_support.py
+++ nexinfosys/models/musiasem_methodology_support.py
@@ -48,6 +48,8 @@ class GUID(TypeDecorator):
     """
     impl = CHAR
 
+    cache_ok = True
+
     def load_dialect_impl(self, dialect):
         if dialect.name == 'postgresql':
             return dialect.type_descriptor(UUID())
